28aa6c0d0593ebba7ad864ebd856f390c7938ac2
Riya Patil <riyapatil2301@gmail.com>
2026-09-21 20:02:17 +0530

    Day 3: Binary Search

---
+++ Day3/BinarySearch.java
@@ -0,0 +1,35 @@
+public class BinarySearch {
+
+    public static int search(int[] arr, int target) {
+
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == target) {
+                return mid;
+            }
+
+            if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {2, 5, 8, 12, 16, 23, 38};
+        int target = 16;
+
+        int result = search(arr, target);
+
+        System.out.println("Target index: " + result);
+    }
+}
